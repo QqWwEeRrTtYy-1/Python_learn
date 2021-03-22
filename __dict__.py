36cc4e5efa7031a37be13fbd225400c17ffbda2b
QqWwEeRrTtYy-1 <71389292+QqWwEeRrTtYy-1@users.noreply.github.com>
2021-03-22 22:11:01 +0300

    Add files via upload

---
+++ __dict__.py
@@ -0,0 +1,7 @@
+my_dict = {
+	"Name" : "Garry",
+	"Surname" : "Osborn",
+	"Age" : 23,
+}
+
+print("Hello! my name is {0} {1} and i am {2} years old".format(my_dict["Name"], my_dict["Surname"], my_dict["Age"]))
